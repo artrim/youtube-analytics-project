74c44c22e0a81ab1435440f93550ecf44081e6a8
Artur Khamidulliun <artrimskiy@gmail.com>
2024-02-01 02:25:51 +0500

    correct self.url

--- src/channel.py
+++ src/channel.py
@@ -15,7 +15,7 @@ class Channel:
         self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
         self.description = self.channel['items'][0]['snippet']['description']
-        self.url = "https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A"
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
         self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
         self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
         self.view_count = self.channel['items'][0]['statistics']['viewCount']
