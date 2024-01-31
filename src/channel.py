552d77d5b030904652ba0548e9f0cd844779d099
Artur Khamidulliun <artrimskiy@gmail.com>
2024-02-01 03:16:27 +0500

    write code for homework 3

--- src/channel.py
+++ src/channel.py
@@ -47,3 +47,27 @@ class Channel:
     def get_service(cls):
         """Возвращает объект для работы с YouTube API"""
         return cls.youtube
+
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __gt__(self, other):
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __ge__(self, other):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+    def __lt__(self, other):
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __le__(self, other):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+    def __eq__(self, other):
+        return int(self.subscriber_count) == int(other.subscriber_count)
