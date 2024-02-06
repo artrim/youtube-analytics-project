7d5296db2e97567515afa67a701eb654117cad09
Artur Khamidulliun <artrimskiy@gmail.com>
2024-02-07 01:38:40 +0500

    create video.py and write code for homework 4

---
+++ src/video.py
@@ -0,0 +1,31 @@
+import os
+from googleapiclient.discovery import build
+
+
+class Video:
+    api_key: str = os.getenv("YOUTUBE_API_KEY")
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, video_id):
+        self.video_id = video_id
+        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                         id=self.video_id
+                                                         ).execute()
+        self.video_title: str = self.video_response['items'][0]['snippet']['title']
+        self.url_video = f'https://youtu.be/{self.video_id}'
+        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return self.video_title
+
+
+class PLVideo(Video):
+
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
+        self.playlist_videos = self.youtube.playlistItems().list(playlistId=self.playlist_id,
+                                                                 part='contentDetails',
+                                                                 maxResults=50,
+                                                                 ).execute()
