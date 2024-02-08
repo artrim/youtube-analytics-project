412c1fef8cbcda80e41e438e1c32e9e318f644b1
Artur Khamidulliun <artrimskiy@gmail.com>
2024-02-09 04:40:21 +0500

    correct video.py

--- src/video.py
+++ src/video.py
@@ -1,14 +1,11 @@
-import os
-from googleapiclient.discovery import build
+from src.service import Service
 
 
-class Video:
-    api_key: str = os.getenv("YOUTUBE_API_KEY")
-    youtube = build('youtube', 'v3', developerKey=api_key)
+class Video(Service):
 
     def __init__(self, video_id):
         self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+        self.video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                          id=self.video_id
                                                          ).execute()
         self.video_title: str = self.video_response['items'][0]['snippet']['title']
@@ -25,7 +22,7 @@ class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
-        self.playlist_videos = self.youtube.playlistItems().list(playlistId=self.playlist_id,
+        self.playlist_videos = self.get_service().playlistItems().list(playlistId=self.playlist_id,
                                                                  part='contentDetails',
                                                                  maxResults=50,
                                                                  ).execute()
