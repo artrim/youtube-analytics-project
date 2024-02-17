97d7ae9713ea98b9361f23ea3fca46f4b29611fa
Artur Khamidulliun <artrimskiy@gmail.com>
2024-02-17 20:13:49 +0500

    write code in video.py for homework 6

--- src/video.py
+++ src/video.py
@@ -5,16 +5,23 @@ class Video(Service):
 
     def __init__(self, video_id):
         self.video_id = video_id
-        self.video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=self.video_id
-                                                         ).execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.url_video = f'https://youtu.be/{self.video_id}'
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=self.video_id
+                                                             ).execute()
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+            self.url_video = f'https://youtu.be/{self.video_id}'
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_response = None
+            self.title = None
+            self.url_video = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
